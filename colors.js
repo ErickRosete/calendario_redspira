f1d1e95468ea34923d918ac95f95f0441edd7b97
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-08-19 14:04:48 -0700

    initial commit

---
+++ colors.js
@@ -0,0 +1,6 @@
+const green = ["#a5d6a7", "#81c784", "#66bb6a", "#4caf50", "#43a047"]; // 00 - 50
+const yellow = ["#fff59d", "#fff176", "#ffee58", "#ffeb3b", "#fdd835"]; // 51 - 100
+const orange = ["#ffcc80", "#ffb74d", "#ffa726", "#ff9800", "#fb8c00"]; // 101 - 150
+const red = ["#ef9a9a", "#e57373", "#ef5350", "#f44336", "#e53935"]; // 151 - 200
+const darkPink = ["#e91e63", "#d81b60", "#c2185b","#ad1457", "#880e4f"]; // 201 - 300
+const darkRed = ["#ba000d", "#ab000d","#9a0007", "#8e0000", "#7f0000"]; // 301 - 500
